6730cd3a7705aa51eb453a4876d4fd0d8aaead9e
Galefury <31698062+Galefury@users.noreply.github.com>
2020-08-15 23:44:53 +0200

    Update Userscript description
    Also test auto-update :)

--- AutoScript.user.js
+++ AutoScript.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Synergism Ascension Automator
+// @description  Automates Ascensions in the game Synergism, 1.011 testing version. May or may not work before ascension.
 // @namespace    Galefury
-// @version      1.7.1
+// @version      1.7.1.1
 // @downloadURL  https://raw.githubusercontent.com/Galefury/synergism-automation/master/AutoScript.user.js
 // @author       Galefury
 // @match        https://v1011testing.vercel.app/
